03df699fced579f21e230a8c5020999778e9b79a
jenli36 <lcy99@uw.edu>
2018-04-13 22:12:07 -0700

    finished

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -5,22 +5,32 @@
 library("dplyr")
 
 # Install (if needed) and load the "fueleconomy" package
-#install.packages('devtools')
-#devtools::install_github("hadley/fueleconomy")
+install.packages('devtools')
+devtools::install_github("hadley/fueleconomy")
 library(fueleconomy)
 
 # Which 2015 Acura model has the best hwy MGH? (Use dplyr, but without method
 # chaining or pipes--use temporary variables!)
-
+acuras <- filter(vehicles, make == 'Acura', year == 2015)
+best_acura <- filter(acuras, hwy == max(hwy))
+best_model <- select(best_acura, model)
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr, nesting functions)
+best_model <- select(
+  filter(
+    filter(vehicles, make == 'Acura', year == 2015), hwy == max(hwy)
+  ), model
+)
 
 
 # Which 2015 Acura model has the best hwy MPG? (Use dplyr and the pipe operator)
-
+best_model <- filter(vehicles, make == 'Acura', year == 2015) %>%
+  filter(hwy == max(hwy)) %>%
+  select(model)
 
 
 ### Bonus
 
 # Write 3 functions, one for each approach.  Then,
 # Test how long it takes to perform each one 1000 times
+
